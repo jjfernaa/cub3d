a8f4e8e6663f3672b32734d110e0627848a2ae7f
Juan Jose Fernandez <juan-jof@c2r4s4.42malaga.com>
2025-11-04 18:01:10 +0100

    include - srcs add

---
+++ includes/cub3d.h
@@ -0,0 +1,16 @@
+#ifndef	CUB3D_h
+# define CUB3D_h
+
+#include "MLX42/MLX42.h"
+#include "libft.h"
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct	s_game
+{
+	mlx_t	*mlx;
+
+}	t_game;
+
+#endif
---
+++ srcs/main.c
@@ -0,0 +1,12 @@
+#include "cub3d.h"
+
+int	main(int argc, char **argv)
+{
+	t_game	*game;
+
+	if (argc != 2)
+		// funcion de error
+	ft_memset(&game, 0, sizeof(t_game));
+	return (0);
+
+}
