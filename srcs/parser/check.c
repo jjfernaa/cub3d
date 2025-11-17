cf2d372a05c9f847f059a9a97d6386386119a6e1
lauraginer <lauginmat@gmail.com>
2025-11-17 17:18:24 +0100

    upload, trying to fix the parser of map

--- srcs/parser/check.c
+++ srcs/parser/check.c
@@ -49,26 +49,30 @@ static int	is_map_line(char *line)
 	i = 0;
 	while(line[i] && (line[i] >= 9 && line[i] <= 32))
 		i++;
-	if(line[i] == '1' || line[i] == '0')
+	if(line[i] == '1' || line[i] == '0') //la primera linea como 
 		return(1);
 	return (0);
 }
 
-int	check_valid_chars(char *line) //no estoy al 100% segura si esto es correcto
+int	check_valid_chars(char *line) 
 {
 	int	i;
 
 	if(is_config_line(line))
 		return(0); 
-	if(!is_map_line(line)) //en el momento en el que empiece a contar 0 o 1 ya estamos en el mapa
-		return(0);
+	if(!is_map_line(line)) 
+	{
+		if(line[0] == '\0' || line[0] == '\n')
+			return(0);
+		return(print_error("Error: Invalid line of map\n"));
+	}
 	i = 0;
 	while (line[i])
 	{
 		if (line[i] != '1' && line[i] != '0' && line[i] != 'N' && line[i] != 'S'
 			&& line[i] != 'E' && line[i] != 'W' && line[i] != ' '
 			&& line[i] != '\t' && line[i] != '\n')
-			return (print_error("Error: Invalid map character\n"));
+			return(print_error("Error: Invalid map character\n"));
 		i++;
 	}
 	return (0);
