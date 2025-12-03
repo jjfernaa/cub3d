5e670db16f716c63b9f05ecc48520ff621c736f4
Juan Jose Fernandez <juan-jof@c3r8s6.42malaga.com>
2025-12-03 20:50:02 +0100

    norminette and header add

--- srcs/execution/raycast_texture.c
+++ srcs/execution/raycast_texture.c
@@ -1,24 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   raycast_texture.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: juan-jof <juan-jof@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/03 20:14:34 by juan-jof          #+#    #+#             */
+/*   Updated: 2025/12/03 20:14:35 by juan-jof         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../../includes/cub3d.h"
 
 void	calculate_wall_x(t_game *game, t_ray *ray)
 {
-	if (ray->side == 0) // Pared vertical (E/W)
+	if (ray->side == 0)
 		ray->wall_x = game->player.y + ray->perp_wall_dist * ray->ray_dir_y;
-	else // Paref horizontal (N/S)
+	else
 		ray->wall_x = game->player.x + ray->perp_wall_dist * ray->ray_dir_x;
-	ray->wall_x -= floor(ray->wall_x); // Parete decimal(0.0 - 1.0)
+	ray->wall_x -= floor(ray->wall_x);
 }
 
 void	select_texture(t_game *game, t_ray *ray)
 {
-	if (ray->side == 0) // pared vertical (E/W)
+	if (ray->side == 0)
 	{
 		if (ray->ray_dir_x > 0)
 			ray->texture = game->textures.east;
 		else
 			ray->texture = game->textures.west;
 	}
-	else // pared horizontal (N/S)
+	else
 	{
 		if (ray->ray_dir_y > 0)
 			ray->texture = game->textures.south;
@@ -30,7 +42,7 @@ void	select_texture(t_game *game, t_ray *ray)
 void	calculate_tex_x(t_ray *ray)
 {
 	ray->tex_x = (int)(ray->wall_x * (double)ray->texture->width);
-	if ((ray->side == 0 && ray->ray_dir_x > 0) 
+	if ((ray->side == 0 && ray->ray_dir_x > 0)
 		|| (ray->side == 1 && ray->ray_dir_y < 0))
 		ray->tex_x = ray->texture->width - ray->tex_x - 1;
 }
